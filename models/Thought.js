019e8bcc48efb3a7f11080bd706863c3d7443867
shellby.miller <shellby.miller@gmail.com>
2023-03-07 20:36:24 -0500

    added thought model based on criteria

---
+++ models/Thought.js
@@ -0,0 +1,81 @@
+const { Schema, model, Types } = require('mongoose');
+// Moment module to format  timstamp
+const moment = require("moment");
+
+// Reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody:{
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) =>
+                moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// Schema to create Thoght model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+      moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
+    },
+    username: 
+      {
+        type: String,
+        required: true
+      },
+
+// Array of nested docuemnts created with reactionSchema
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+  }
+);
+
+
+// Get total reaction count
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length
+});
+
+// Initialize Thought model
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
