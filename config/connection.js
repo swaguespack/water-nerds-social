d9eeb4da06e8bbd1ee3823f509f3f0c1b5fa1011
shellby.miller <shellby.miller@gmail.com>
2023-03-07 19:36:54 -0500

    added user model based on criteria

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 // Environment Variable & wrap Mongoose around local connection to MongoDB
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/waterSocial'
-connect(connectionString, {
+connect(process.env.MONGODB_URI || 'mongodb://localhost/water-social', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
